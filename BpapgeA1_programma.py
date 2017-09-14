bab5cec07565993cf6784243b1b484d7f90137bb
nilssteinz <nils.steinz@hotmail.com>
2017-09-14 17:47:57 +0200

    Add files via upload

--- BpapgeA1_programma.py
+++ BpapgeA1_programma.py
@@ -3,15 +3,19 @@ import subprocess
 
 
 def dw_files():
-    try:
-        os.system('mkdir bestanden')
-        os.system('mv opdracht ./bestanden/opdracht')
-        os.system('mv bpapge_seq_a1 ./bestanden/bpapge_seq_a1')
-    finally:
-        subprocess.run(['bash', 'opdracht'], cwd='bestanden')
-        os.system('mv ./bestanden/opdracht opdracht')
-        os.system('mv ./bestanden/bpapge_seq_a1 bpapge_seq_a1')
-        print('Downloading done\nBlast done')
+    """
+
+
+
+    :return: Niks
+    """
+    os.system('mkdir bestanden')
+    os.system('mv downloadblast ./bestanden/downloadblast')
+    os.system('mv bpapge_seq_a1 ./bestanden/bpapge_seq_a1')
+    subprocess.run(['bash', 'downloadblast'], cwd='bestanden')
+    os.system('mv ./bestanden/downloadblast downloadblast')
+    os.system('mv ./bestanden/bpapge_seq_a1 bpapge_seq_a1')
+    print('Downloading done\nBlast done')
 
 
 def fill_seq(naam):
@@ -36,15 +40,70 @@ def test():
             #     print(line.strip(' ').split('"')[1])
 
 
-def download_gb_data(namen_file, database_soort):
+def download_gb_data_FI(namen_file, database_soort):
     os.system('mkdir temp')
+    os.system('mkdir temp/{}'.format(database_soort))
+    adderes = os.getcwd()+'/temp/{}'.format(database_soort)
     with open(namen_file, 'r') as namen_bestand:
         namen = namen_bestand.read().strip()
         for naam in namen.split('\n'):
-            print('{} wordt gedownload'.format(naam))
-            subprocess.run(['wget',
-                            "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db={0}&id={1}&rettype=gb&retmode=text".format(
-                                database_soort, naam), '-O {}.txt'.format(naam), ' -P temp/'])
+            download_from_NCBI(database_soort, naam, 'temp')
+
+
+def download_bg_data_dict(data_dict, database_soort):
+    os.system('mkdir temp')
+    os.system('mkdir temp/{}'.format(database_soort))
+    adderes = os.getcwd()+'/temp/{}'.format(database_soort)
+    for i in data_dict:
+        download_from_NCBI(database_soort, data_dict[i][1], 'temp')
+
+def download_from_NCBI(database, id, temp_naam):
+    print('{} wordt gedownload'.format(id))
+    subprocess.run(['wget',
+        "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db={0}&id={1}&rettype=gb&retmode=text".format(
+            database, id), '-O {}.txt'.format(id), '-q'], cwd='{}/{}'.format(temp_naam, database))
+
+
+
+def data_splitten(path, data_dict, database):
+    ID, gene, product = 'null', 'null', 'null'
+    with open(path, 'r') as bg_file:
+        for line in bg_file.read().strip().split('\n'):
+            if database == 'nucleotide':
+                if 'protein_id' in line:
+                    ID = line.split('"')[1]
+            else:
+                if '/coded_by' in line:
+                    ID = line.split('"')[1].split('.')[0]
+            if '/gene' in line:
+                gene = line.split('"')[1]
+            if '/product' in line:
+                product = line.split('"')[1]
+            if 'ORIGIN' in line:
+                data_dict[file] = [database, ID, gene, product]
+
+
+def get_data(database):
+    data_dict = {}
+    path = os.getcwd() + '/temp/{}'.format(database)
+    for file in os.listdir(path):
+
+        ID, gene, product = 'null', 'null', 'null'
+        with open(path + '/' + file, 'r') as bg_file:
+            for line in bg_file.read().strip().split('\n'):
+                if database == 'nucleotide':
+                    if 'protein_id' in line:
+                        ID = line.split('"')[1]
+                else:
+                    if '/coded_by' in line:
+                        ID = line.split('"')[1].split('.')[0]
+                if '/gene' in line:
+                    gene = line.split('"')[1]
+                if '/product' in line:
+                    product = line.split('"')[1]
+                if 'ORIGIN' in line:
+                    data_dict[file] = [database, ID, gene, product]
+    return data_dict
 
 
 def main():
@@ -52,15 +111,19 @@ def main():
     # genen = fill_seq('raw_genen.txt')
     # eiwiten = fill_seq('raw_eiwitten.txt')
     #
+    #
     # for x in genen:
     #     print(x, genen[x])
     # print()
     #
     # for i in eiwiten:
     #     print(i, eiwiten[i])
-    test()
-    #download_gb_data('./bestanden/gevonden_genen.txt', 'nucleotide')
-
+    # test()
+    # download_gb_data('./bestanden/gevonden_genen.txt', 'nucleotide')
+    nucl_data = get_data('nucleotide')
+    # download_bg_data_dict(nucl_data, 'protein')
+    prot_data = get_data('protein')
+    print(prot_data)
     pass
 
 
