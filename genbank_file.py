83694a6a04b5a0dec35622d16c3a4eedd655666a
nils <nils.steinz@hotmail.com>
2017-09-18 16:36:30 +0200

    genbank file

--- genbank_file.py
+++ genbank_file.py
@@ -8,16 +8,32 @@ class Protein_gb_info():
     def __lijst_met_variables(self):
         self.regions = {}
 
+    def get_regions(self):
+        """
+        returnt de regions die in de genbank file staan.
+        :return: Regions Dict.
+        """
+        return self.regions
+
     def __get_info(self):
+        """
+        hoofd functie, dit voert als hij af is alles uit.
+        dus kan je alles uit proberen.
+        :return:
+        """
         for i in self.regions:
             print(i, self.regions[i])
         pass
 
     def _split_protein_regions_test(self):
+        """
+
+        :return:
+        """
         data, region_dict = [], {}
         region = False
         for line in self.file:
-            #print(line)
+            # print(line)
             if 'Region' in line:
                 region = True
             if region:
@@ -29,5 +45,11 @@ class Protein_gb_info():
                 data = []
 
 
-
-test = Protein_gb_info('temp/protein/XP_007072912.1.txt')
+if __name__ == '__main__':
+    import os
+    os.system('mkdir bestanden')
+    os.system(
+        'wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db={0}&id={1}&rettype=gb&retmode=text" -O ./bestanden/{1}.txt -q'.format(
+            'nucleotide', 'XM_007079698.2'))
+    test = Protein_gb_info('bestanden/XP_007072912.1.txt')
+    test.get_regions()
