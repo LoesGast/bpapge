f4bfeb28a4c73c99bf84443b3dcf615af3ef13c0
nils <nils.steinz@hotmail.com>
2017-09-19 11:03:31 +0200

    genbank file update
    ec_nummers erbij

--- blasten.sh
+++ blasten.sh
@@ -4,3 +4,5 @@ formatdb -i tiger_genoom.fa -p F
 blastall -p blastn -d tiger_genoom.fa  -i bpapge_seq_a1 -m8 > output_blasten.txt
 
 cat output_blasten.txt | awk '{if($11 == "0.0"){print $1, $2}}' | sort  | tr '|' ' ' | awk '{print $4}'
+
+# kegg naam :ptg
--- genbank_file.py
+++ genbank_file.py
@@ -1,12 +1,23 @@
+import requests
+import sys
+
+
 class Protein_gb_info():
+
     def __init__(self, file_naam):
         self.file = open(file_naam, 'r').readlines()
         self.__lijst_met_variables()
-        self._split_protein_regions_test()
+        self._split_protein_regions()
         self.__get_info()
+        self._ec_getter()
 
     def __lijst_met_variables(self):
+        self.locus = self.file[0].split('      ')[1].strip(' ')
+        self.name = self.file[1].split(':')[-1].split('[')[0].strip(' ')
         self.regions = {}
+        self.site = {}
+        self.sequence = ''
+        self.ec_nummer = ''
 
     def get_regions(self):
         """
@@ -15,22 +26,55 @@ class Protein_gb_info():
         """
         return self.regions
 
+    def get_sequence(self):
+        return self.sequence
+
+    def get_sites(self):
+        return self.site
+
+    def get_name(self):
+        return self.name
+
+    def get_ec_nummer(self):
+        return self.ec_nummer
+
     def __get_info(self):
         """
         hoofd functie, dit voert als hij af is alles uit.
         dus kan je alles uit proberen.
         :return:
         """
-        for i in self.regions:
-            print(i, self.regions[i])
-        pass
 
-    def _split_protein_regions_test(self):
+    def _ec_getter(self):
         """
 
         :return:
         """
-        data, region_dict = [], {}
+        ec_nummers = []
+        naam = '%20'.join(self.name.split(' '))
+        requestURL = "https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&protein={}".format(naam)
+        r = requests.get(requestURL, headers={"Accept": "application/xml"})
+
+        if not r.ok:
+            r.raise_for_status()
+            sys.exit()
+
+        responseBody = r.text.split('<')
+        for line in responseBody:
+            if 'ecNumber evidence' in line:
+                ec_nummers += [line.split('>')[1]]
+        if ec_nummers == []:
+            self.ec_nummer = 'none'
+        else:
+            self.ec_nummer= set(ec_nummers)
+
+
+    def _split_protein_regions(self):
+        """
+
+        :return:
+        """
+        data = []
         region = False
         for line in self.file:
             # print(line)
@@ -44,12 +88,15 @@ class Protein_gb_info():
                 region = False
                 data = []
 
+    def __str__(self):
+        return self.locus
+
 
 if __name__ == '__main__':
     import os
+    name = 'XP_007072912.1'
     os.system('mkdir bestanden')
     os.system(
         'wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db={0}&id={1}&rettype=gb&retmode=text" -O ./bestanden/{1}.txt -q'.format(
-            'nucleotide', 'XM_007079698.2'))
-    test = Protein_gb_info('bestanden/XP_007072912.1.txt')
-    test.get_regions()
+            'protein', name))
+    test = Protein_gb_info(('bestanden/'+ name + '.txt'))
